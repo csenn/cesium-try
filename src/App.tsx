7693aaa1a8975cb24469008507ea125225be4382
Craig Sennabaum <csennabaum@gmail.com>
2025-01-26 08:24:17 -0700

    add time updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,33 @@ import {
   Polyline,
   CesiumComponentRef,
   RootEventTarget,
+  Clock,
 } from "resium";
 import {
   Cartesian3,
   Color,
   Viewer as CesiumViewer,
   PointPrimitive as CesiumPointPrimitive,
+  Clock as CesiumClock,
+  NearFarScalar,
+  JulianDate,
   // GeoJsonDataSource,
 } from "cesium";
 import { APP_DATE_START, getSatelliteData, ISatellite } from "./loadData";
 // import satData from "./sat-pos.json";
-import { useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { generateOrbitPoints, getPositionAtTime } from "./orbitUtils";
-import { compact } from "lodash";
+import { compact, throttle } from "lodash";
 
 // import czml from "./simple-czml.json";
 
 // const center = Cartesian3.fromDegrees(-75.59777, 40.03883);
 
+const pointScaler = new NearFarScalar(1.5e6, 1, 4.2e7, 0.5);
+
 function App() {
   const viewerRef = useRef<CesiumComponentRef<CesiumViewer>>(null);
+  const [pointTime, setPointTime] = useState(APP_DATE_START);
 
   const [selectedSat, setSelectedSat] = useState<ISatellite | null>(null);
   // const viewerRef = useRef<typeof Viewer>(null);
@@ -53,7 +60,7 @@ function App() {
   const pointData = useMemo(() => {
     return compact(
       satData.map((sat) => {
-        const position = getPositionAtTime(sat, APP_DATE_START);
+        const position = getPositionAtTime(sat, pointTime);
         if (!position) {
           return null;
         }
@@ -65,24 +72,45 @@ function App() {
         };
       }),
     );
-  }, [satData]);
+  }, [pointTime]);
 
-  const pointViews = pointData.map((point) => {
-    return (
-      <PointPrimitive
-        color={Color.YELLOW}
-        position={new Cartesian3(point.x, point.y, point.z)}
-        pixelSize={5}
-        id={point.satId}
-        //  onClick={doSomething}
-      />
-    );
-  });
+  const pointViews = useMemo(() => {
+    return pointData.map((point) => {
+      return (
+        <PointPrimitive
+          color={Color.YELLOW}
+          position={new Cartesian3(point.x, point.y, point.z)}
+          pixelSize={5}
+          id={point.satId}
+          scaleByDistance={pointScaler}
+
+          //  onClick={doSomething}
+        />
+      );
+    });
+  }, [pointData]);
+
+  const handleClockTick = useMemo(
+    () =>
+      throttle((clock: CesiumClock) => {
+        console.log("Throttled clock tick", clock.currentTime);
+        setPointTime(JulianDate.toDate(clock.currentTime));
+      }, 3000),
+    [setPointTime],
+  );
+
+  const onClockTick = useCallback(
+    (clock: CesiumClock) => {
+      handleClockTick(clock);
+    },
+    [handleClockTick],
+  );
 
   // const selectedSat = satData[100]
   const orbitPoints = selectedSat
     ? generateOrbitPoints(selectedSat, APP_DATE_START)
     : [];
+
   const polyline = (
     <PolylineCollection>
       <Polyline
@@ -95,6 +123,11 @@ function App() {
     </PolylineCollection>
   );
 
+  const endTime = useMemo(
+    () => new Date(APP_DATE_START.getTime() + 24 * 60 * 60 * 1000),
+    [APP_DATE_START],
+  );
+
   return (
     <Viewer ref={viewerRef} onClick={onClickViewer}>
       <PointPrimitiveCollection
@@ -103,6 +136,12 @@ function App() {
         {pointViews}
       </PointPrimitiveCollection>
       {polyline}
+      <Clock
+        startTime={JulianDate.fromDate(APP_DATE_START)}
+        currentTime={JulianDate.fromDate(APP_DATE_START)}
+        stopTime={JulianDate.fromDate(endTime)}
+        onTick={onClockTick}
+      />
     </Viewer>
   );
 }
